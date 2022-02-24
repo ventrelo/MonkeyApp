97a4970b577feeafd97dd0f1073b28c4afc89b2f
david <david@DESKTOP-N34UK9U>
2022-02-24 15:40:44 +0200

    changed project to python

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_member_join(member):
+    await member.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
+
+client.run(TOKEN)
