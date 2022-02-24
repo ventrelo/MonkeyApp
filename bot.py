52385da1c146504e3f101e093e612fa104c5abbb
david <david@DESKTOP-N34UK9U>
2022-02-24 17:53:58 +0200

    Finished into to bots

--- bot.py
+++ bot.py
@@ -1,23 +1,26 @@
 import os
 import discord
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
+from discord.ext import commands
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.members = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!',intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
-
-@client.event
-async def on_member_join(member):
-    await member.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
-
-client.run(TOKEN)
+@bot.command(name='create-channel')
+@commands.has_role('admin')
+async def create_channel(ctx, channel_name='real-python'):
+    guild = ctx.guild
+    existing_channel = discord.utils.get(guild.channels, name=channel_name)
+    if not existing_channel:
+        print(f'Creating a new channel: {channel_name}')
+        await guild.create_text_channel(channel_name)
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send('You do not have the correct role for this command.')
+bot.run(TOKEN)
